a6affe434136c289f68608a7b91768cc3f9e2370
chris <nuke82@nate.com>
2018-01-16 17:33:49 +0900

    venv 추가

--- exam01.py
+++ exam01.py
@@ -1,2 +1,4 @@
-# vim .... fuck
-# 미친 빔
+from urllib.request import urlopen
+#Retrieve HTML string from the URL
+html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
+print(html.read())
